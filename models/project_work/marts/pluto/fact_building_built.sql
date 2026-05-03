7b81829fb3420b017211dce0d61acc448e7b3286
Gil Lu <GIL.LU@baruchmail.cuny.edu>
2026-05-03 02:31:46 +0000

    change building age category to reference fact building built

--- models/project_work/marts/pluto/fact_building_built.sql
+++ models/project_work/marts/pluto/fact_building_built.sql
@@ -5,21 +5,24 @@ WITH pluto AS (
    SELECT * FROM {{ ref('stg_pluto') }}
 ),
 
+age_categories AS (
+   SELECT * FROM {{ ref('dim_building_age_category') }}
+),
+
 fact_building AS (
    SELECT
        {{ dbt_utils.generate_surrogate_key(['p.bbl']) }} AS building_fact_key,
 
-       -- Foreign keys to dimensions
        {{ dbt_utils.generate_surrogate_key(['p.bbl']) }} AS location_key,
        {{ dbt_utils.generate_surrogate_key(['p.bbl']) }} AS building_key,
-       {{ dbt_utils.generate_surrogate_key(['p.building_age_category']) }} AS age_category_key,
+       ac.age_category_key,                                     -- ← now references the dim
 
-       -- Year measures
        CAST(p.year_built AS INT64) AS year_built,
        CAST(p.year_alter1 AS INT64) AS yearalter1,
        CAST(p.year_alter2 AS INT64) AS yearalter2
 
    FROM pluto p
+   LEFT JOIN age_categories ac ON p.building_age_category = ac.age_category_name
    WHERE p.bbl IS NOT NULL
 )
 
